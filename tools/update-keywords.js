3af5a1b243de96cadc553f1012d3dbff8482a994
Alhadis <gardnerjohng@gmail.com>
2016-05-13 17:21:18 +1000

    Update logic and grammar to include Vim options

--- tools/update-keywords.js
+++ tools/update-keywords.js
@@ -6,20 +6,49 @@ const fs     = require("fs");
 
 const target = "grammars/viml.cson";
 
-readStdin().then(input => {
-	fs.writeFileSync(target, fs.readFileSync(target)
-		.toString()
-		.replace(
-			/(^[\x20\t]*commands:\n(?:.+\n)*?\s+match:\s*"[^(]*\().+?(\).+$)/im,
-			`$1${ compileKeywords(input) }$2`
-		));
+readStdin().then(vimSyntax => {
+	const types = {
+		commands:   "vimCommand",
+		stdPlugins: "vimStdPlugin",
+		options:    "vimOption"
+	};
+	
+	const fileData = updateKeywords(types, vimSyntax, fs.readFileSync(target).toString());
+	fs.writeFileSync(target, fileData);
 });
 
 
-function compileKeywords(input){
+/**
+ * Parse Vim's syntax file for defined keywords and update Atom's syntax accordingly.
+ *
+ * @param {Object} types - A dictionary of named keyword-types
+ * @param {String} vimSyntax - Contents of Vim's syntax file
+ * @param {String} atomSyntax - Contents of Atom's grammar file
+ * @return {String} Atom's syntax file, updated with the parsed keywords
+ */
+function updateKeywords(types, vimSyntax, atomSyntax){
+	for(let i in types){
+		let vimName  = types[i];
+		let pattern  = new RegExp(`(^[\\x20\\t]*${i}:\\n(?:.+\\n)*?\\s+match:\\s*"[^(]*\\().+?(\\).+$)`, "im");
+		let keywords = compileKeywords(vimName, vimSyntax);
+		atomSyntax   = atomSyntax.replace(pattern, `$1${keywords}$2`);
+	}
+	
+	return atomSyntax;
+}
+
+
+/**
+ * Construct a pipe-separated list of keywords found in Vim's syntax file.
+ *
+ * @param {String} type - The second argument of the "syn" call to match in Vim's syntax file
+ * @param {String} input - Contents of Vim syntax
+ * @return {String}
+ */
+function compileKeywords(type, input){
 	let matches = [];
 	input.replace(
-		/^\s*syn\s+keyword\s+vimCommand\s+contained\s+(.+)$/gm,
+		new RegExp("^\\s*syn\\s+keyword\\s+" + type + "\\s+contained\\s+(.+)$", "gm"),
 		(line, match) => matches.push(match)
 	);
 	
@@ -36,6 +65,11 @@ function compileKeywords(input){
 }
 
 
+/**
+ * Helper function to return the entirety of standard input.
+ *
+ * @return {Promise}
+ */
 function readStdin(){
 	return new Promise(resolve => {
 		let input = "";
